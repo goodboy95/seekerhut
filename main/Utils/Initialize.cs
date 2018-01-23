13d6bfeafd53e3f734cfe47bca3771798be7853f
goodboy95 <501217662@qq.com>
2018-01-23 21:10:45 +0800

    修复数据库初始化的bug

--- main/Utils/Initialize.cs
+++ main/Utils/Initialize.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Dao;
 using Domain.Entity;
 using Microsoft.EntityFrameworkCore;
@@ -10,12 +11,15 @@ namespace Utils
         {
             c.Database.EnsureCreated();
             //c.Database.Migrate();
-            c.CommonConfig.Add(new CommonConfigEntity{Item = "BlogPageSize", Value = "20"});
-            c.CommonConfig.Add(new CommonConfigEntity{Item = "BlogReplyPageSize", Value = "10"});
-            c.CommonConfig.Add(new CommonConfigEntity{Item = "ForumPostPageSize", Value = "20"});
-            c.CommonConfig.Add(new CommonConfigEntity{Item = "ForumReplyPageSize", Value = "20"});
-            c.CommonConfig.Add(new CommonConfigEntity{Item = "ForumSubReplyPageSize", Value = "10"});
-            c.SaveChanges();
+            if (!c.CommonConfig.Any())
+            {
+                c.CommonConfig.Add(new CommonConfigEntity{Item = "BlogPageSize", Value = "20"});
+                c.CommonConfig.Add(new CommonConfigEntity{Item = "BlogReplyPageSize", Value = "10"});
+                c.CommonConfig.Add(new CommonConfigEntity{Item = "ForumPostPageSize", Value = "20"});
+                c.CommonConfig.Add(new CommonConfigEntity{Item = "ForumReplyPageSize", Value = "20"});
+                c.CommonConfig.Add(new CommonConfigEntity{Item = "ForumSubReplyPageSize", Value = "10"});
+                c.SaveChanges();
+            }
         }
     }
 }
